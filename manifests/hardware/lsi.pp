4f0064610b375c25b4852bced3f29aa7ea10ea94
Markus Rekkenbeil <markus@bionix-it.de>
2013-06-27 22:39:19 +0200

    Add LSI megacli tool and remove old and broken megactl
    Signed-off-by: Markus Rekkenbeil <markus@bionix-it.de>

--- manifests/hardware/lsi.pp
+++ manifests/hardware/lsi.pp
@@ -1,9 +1,36 @@
-class raid::hardware::threeware {
-  file { '/usr/sbin/megactl':
+class raidtool::hardware::lsi {
+  file { '/opt':
+    ensure => directory
+  }
+  file { '/opt/megacli.deb':
     ensure => present,
-    source => "puppet:///modules/raidtool/lsi/megactl",
+    source => "puppet:///modules/raidtool/lsi/megacli_8.07.10-2_all.deb",
     owner => root,
     group => root,
-    mode => 0555,
+    mode => 0550,
+    require => File['/opt']
+  }
+  package { "megacli":
+    ensure  =>  latest,
+    source => "/opt/megacli.deb",
+    provider => dpkg,
+    require => File['/opt', '/opt/megacli.deb']
+  }
+  case $architecture {
+    amd64: {
+      file { '/usr/sbin/megacli':
+        ensure => link,
+        target => '/opt/MegaRAID/MegaCli/MegaCli64'
+      }
+    }
+    i386: {
+      file { '/usr/sbin/megacli':
+        ensure => link,
+        target => '/opt/MegaRAID/MegaCli/MegaCli'
+      }
+    }
+    default: {
+      notice "Unsupported architecture - $::architecture"
+    }
   }
 }
