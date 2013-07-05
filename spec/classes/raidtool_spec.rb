31c694336460b3f027cc5d07e5a4d17e9f3bafac
Markus Rekkenbeil <markus@bionix-it.de>
2013-07-05 13:46:35 +0200

    Add facter tests for supported version 1.6.x / 1.7.x and unsupported version 1.4.x
    Signed-off-by: Ingo Oeser <ingo@jimdo.com>

--- spec/classes/raidtool_spec.rb
+++ spec/classes/raidtool_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'raidtool', :type => :class do
 
-  context "with supported facter version" do
+  context "with supported facter version 1.6.x" do
     let(:facts) {
       {
       :fqdn => 'somehost.domaindomain.org',
@@ -10,7 +10,31 @@ describe 'raidtool', :type => :class do
     }
     }
 
-    it "detect facterversion" do
+    it "detect facterversion 1.6.x" do
+      should include_class('raidtool::detect')
+    end
+  end
+  context "with unsupported facter version" do
+    let(:facts) {
+      {
+      :fqdn => 'somehost.domaindomain.org',
+      :facterversion => '1.4.3',
+    }
+    }
+
+    it "detect unsupported facterversion" do
+      should_not include_class('raidtool::detect')
+    end
+  end
+  context "with supported facter version 1.7.x" do
+    let(:facts) {
+      {
+      :fqdn => 'somehost.domaindomain.org',
+      :facterversion => '1.7.1',
+    }
+    }
+
+    it "detect supported facterversion 1.7.x" do
       should include_class('raidtool::detect')
     end
   end
